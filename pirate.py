dd3f3753194f88c56c3605ce0d090391c4e40d28
Alex Weston <westona@janelia.hhmi.org>
2016-07-18 12:34:45 -0400

    SQUACK!

--- pirate.py
+++ pirate.py
@@ -8,3 +8,8 @@ print "I love pasta"
 with open('temp.txt', 'w') as f:
     for i in range(5):
         f.write("walk three paces left")
+
+
+
+ajsflk; aijoadsjfl;ajsdlfkjal;shfldk;sahf;ahsf
+lahsdfklkahfk;askfla'jf;lafjl;af
